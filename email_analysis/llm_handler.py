03f885d5aff0e0e71f8209ee7612443ddf84e4e6
mAc BoOk pRo <dotenterprises@192.168.1.24>
2025-07-11 19:10:20 +0500

    .env key changes

--- email_analysis/llm_handler.py
+++ email_analysis/llm_handler.py
@@ -12,13 +12,13 @@ class LLMHandler:
         if not GROQ_API_KEY:
             raise ValueError("GROQ_API_KEY not found in environment variables")
         
-        self.client = Groq(api_key=GROQ_API_KEY)
-        self.model = GROQ_MODEL
+        # self.client = Groq(api_key=GROQ_API_KEY)
+        # self.model = GROQ_MODEL
 
-        # self.client =OpenAI(
-        #     base_url="https://openrouter.ai/api/v1",
-        #     api_key=OPEN_ROUTER_API
-        # )
+        self.client =OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=OPEN_ROUTER_API
+        )
         #
         # self.model = LLAMA_MODEL
     
